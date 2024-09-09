4b3bf60d2de292df342083cd266a7c0d940cb7fb
ryuhungsun <67034145+ryuhungsun@users.noreply.github.com>
2024-09-09 11:33:28 +0900

    Delete src directory

--- src/main/java/com/jusung/app/service/EmpService.java
+++
@@ -1,9 +0,0 @@
-package com.jusung.app.service;
-
-import com.jusung.app.dto.EmpDTO;
-import com.jusung.app.dto.ResultDTO;
-
-public interface EmpService {
-	public ResultDTO empList(EmpDTO empDto); 
-	public ResultDTO selectEmp(EmpDTO empDto); 
-}
